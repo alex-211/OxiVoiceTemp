81649759e849428bfb05cffd89c6f1c6ac5b7712
Alessandro Porpiglia <alessandropor@icloud.com>
2023-06-05 22:53:23 +0200

    Modified files to work
    - Added mongoDB
    - Added token
    ----
    Need to finish: config_version & hosting

--- src/main/java/dev/starless/maggiordomo/config/ConfigEntry.java
+++ src/main/java/dev/starless/maggiordomo/config/ConfigEntry.java
@@ -4,8 +4,8 @@ import dev.starless.maggiordomo.Main;
 
 public enum ConfigEntry {
 
-    CONFIG_VERSION("config_version", Main.getVersion()),
-    TOKEN("token", "discord-bot-token"),
+    CONFIG_VERSION("config_version", Main.getVersion()), // Config version needs to be decided, I have no idea yet, will read documentation 
+    TOKEN("MTExNDY5NzE1OTgwMDAwODg4NA.GeVqrD.geq19m7PKSLw4aKZWnNgwMBgJHlyh5IR5eNzR8", "discord-bot-token"),
     MONGO("mongo", "mongodb://");
 
     private final String path;
