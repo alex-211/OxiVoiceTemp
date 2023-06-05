b314644adb37f6fd3572219c93e31f89915cd235
Alessandro Porpiglia <alessandropor@icloud.com>
2023-06-05 22:56:22 +0200

    Some important changes

--- src/main/java/dev/starless/maggiordomo/config/ConfigEntry.java
+++ src/main/java/dev/starless/maggiordomo/config/ConfigEntry.java
@@ -5,7 +5,7 @@ import dev.starless.maggiordomo.Main;
 public enum ConfigEntry {
 
     CONFIG_VERSION("config_version", Main.getVersion()), // Config version needs to be decided, I have no idea yet, will read documentation 
-    TOKEN("MTExNDY5NzE1OTgwMDAwODg4NA.GeVqrD.geq19m7PKSLw4aKZWnNgwMBgJHlyh5IR5eNzR8", "discord-bot-token"),
+    TOKEN("", "discord-bot-token"), // look for it in your gists
     MONGO("mongo", "mongodb://");
 
     private final String path;
